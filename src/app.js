c0d8f246a88ab8040940874ef6422fb937ff614a
Shubham Golhar <shubham.golhar@globallogic.com>
2025-07-03 15:25:29 +0530

    created middleware

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from DevTinder");
+app.use("/testAdmin", (req, res, next) => {
+  console.log("checking admin");
+
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyzxxx"; // Simulating admin check
+  if (!isAdminAuthorized) {
+    res.send("Unauthorized access to admin route");
+  } else {
+    next();
+  }
+}); /////this is the middleware which  will work before each routes
+
+app.get("/testAdmin", (req, res) => {
+  console.log("Admin route accessed");
+  res.send("Admin route is working");
+});
+
+app.post("/testAdmin", (req, res) => {
+  console.log("Admin POST route accessed");
+  res.send("Admin POST route is working");
 });
 
 app.listen(3000, () => {
